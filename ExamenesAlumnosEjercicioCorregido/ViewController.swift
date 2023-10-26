e5f474b9a87c1d398b8ee3858ffd9500b79224a3
ignaciocampillosserrano <ignacio.campillos.serrano@cieep.com>
2023-10-26 12:16:06 +0200

    Initial Commit

---
+++ ExamenesAlumnosEjercicioCorregido/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ExamenesAlumnosEjercicioCorregido
+//
+//  Created by Ignacio Campillos Serrano on 26/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
